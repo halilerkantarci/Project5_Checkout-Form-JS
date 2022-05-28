e59811aa4caa63fdc1cf9b96e209e00bd86ea999
halil <halilerk77@icloud.com>
2022-05-28 21:09:54 +0300

    .

--- app.js
+++ app.js
@@ -1,38 +1,50 @@
 function arttir() {
-  var sonuc1 = document.getElementById("sonuc1");
+  let sonuc1 = document.getElementById("sonuc1");
   sonuc1.value = Number(sonuc1.value) + 1;
   document.getElementsByClassName("product-total")[0].innerHTML =
     "Product Total:" + sonuc1.value * 54.99 + "$";
+  let g = sonuc1.value * 54.99;
 }
+
 function azalt() {
-  var sonuc1 = document.getElementById("sonuc1");
+  let sonuc1 = document.getElementById("sonuc1");
   sonuc1.value = Number(sonuc1.value) - 1;
   document.getElementsByClassName("product-total")[0].innerHTML =
     "Product Total:" + sonuc1.value * 54.99 + "$";
+  let first2 = sonuc1.value * 54.99;
 }
+
 function plus() {
-  var sonuc2 = document.getElementById("sonuc2");
+  let sonuc2 = document.getElementById("sonuc2");
   sonuc2.value = Number(sonuc2.value) + 1;
   document.getElementsByClassName("product-total1")[0].innerHTML =
     "Product Total:" + sonuc2.value * 45.99 + "$";
+
+  return Number(sonuc2.value) * 45.99;
 }
+
 function under() {
-  var sonuc2 = document.getElementById("sonuc2");
+  let sonuc2 = document.getElementById("sonuc2");
   sonuc2.value = Number(sonuc2.value) - 1;
   document.getElementsByClassName("product-total1")[0].innerHTML =
     "Product Total:" + sonuc2.value * 45.99 + "$";
+  let second2 = sonuc2.value * 45.99;
 }
+
 function fazla() {
-  var sonuc3 = document.getElementById("sonuc3");
+  let sonuc3 = document.getElementById("sonuc3");
   sonuc3.value = Number(sonuc3.value) + 1;
   document.getElementsByClassName("product-total2")[0].innerHTML =
     "Product Total:" + sonuc3.value * 74.99 + "$";
+  return Number(sonuc3.value) * 74.99;
 }
+
 function az() {
-  var sonuc3 = document.getElementById("sonuc3");
+  let sonuc3 = document.getElementById("sonuc3");
   sonuc3.value = Number(sonuc3.value) - 1;
   document.getElementsByClassName("product-total2")[0].innerHTML =
     "Product Total:" + sonuc3.value * 74.99 + "$";
+  let third2 = sonuc3.value * 74.99;
 }
 const x = document.getElementsByClassName("remove")[0];
 x.addEventListener(
